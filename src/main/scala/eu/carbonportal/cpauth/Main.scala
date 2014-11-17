ba7b6d5936852ed01fc30e8b8c0849298a278ca2
Oleg Mirzov <oleg.mirzov@gmail.com>
2014-11-18 00:26:31 +0100

    Experimented with Set-Cookie

--- src/main/scala/eu/carbonportal/cpauth/Main.scala
+++ src/main/scala/eu/carbonportal/cpauth/Main.scala
@@ -2,19 +2,31 @@ package eu.carbonportal.cpauth
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
-
 import akka.actor.ActorSystem
 import akka.util.Timeout
 import spray.http.Uri
 import spray.routing.SimpleRoutingApp
+import spray.http.HttpCookie
 
 object Main extends App with SimpleRoutingApp with ProxyDirectives {
 	implicit val system = ActorSystem("cpauth")
 	implicit val timeout: Timeout = Timeout(60.seconds)
 	import scala.concurrent.ExecutionContext.Implicits.global
 
+	val cookie = HttpCookie(
+		name = "testcookie",
+		content = "success",
+//		secure = true,
+		domain = Some(".localhost.local"),
+		path = Some("/"),
+//		maxAge = Some(10.minutes.toSeconds)
+		httpOnly = true
+	)
+	
 	startServer(interface = "::0", port = 8080) {
-		proxyTo(Uri.NamedHost("spray.io"), 80)
+		setCookie(cookie){
+			proxyTo(Uri.NamedHost("oleg.mirzov.com"), 80)
+		}
 	}
 	
 	
