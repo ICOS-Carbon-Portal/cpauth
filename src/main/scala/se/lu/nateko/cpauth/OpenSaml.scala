95dc10e6e5db9d9d9b5d24967588f35236f7751d
Oleg Mirzov <oleg.mirzov@gmail.com>
2014-12-10 00:09:10 +0100

    Added some code for SAML Response analysis

--- src/main/scala/se/lu/nateko/cpauth/OpenSaml.scala
+++ src/main/scala/se/lu/nateko/cpauth/OpenSaml.scala
@@ -1,42 +1,47 @@
 package se.lu.nateko.cpauth
 
-import org.opensaml.xml.Configuration
-import org.opensaml.common.SAMLObjectBuilder
-import org.opensaml.saml2.core.Assertion
-import org.opensaml.xml.XMLObjectBuilderFactory
-import org.opensaml.saml2.metadata.provider.HTTPMetadataProvider
-import java.util.Timer
-import org.apache.commons.httpclient.HttpClient
-import akka.actor.ActorSystem
-import spray.http._
-import spray.client.pipelining._
-import scala.concurrent.Future
-import scala.concurrent.Await
-import scala.concurrent.duration._
-import org.opensaml.xml.parse.StaticBasicParserPool
-import org.opensaml.xml.parse.ParserPool
-import org.opensaml.DefaultBootstrap
-import org.opensaml.xml.XMLObject
-import org.opensaml.saml2.metadata.provider.MetadataProvider
-import org.opensaml.saml2.core.EncryptedAssertion
-import java.security.spec.PKCS8EncodedKeySpec
-import java.security.interfaces.RSAPrivateKey
+import java.io.InputStream
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.security.KeyFactory
-import org.opensaml.xml.security.x509.BasicX509Credential
-import org.opensaml.saml2.encryption.Decrypter
-import org.opensaml.xml.security.keyinfo.StaticKeyInfoCredentialResolver
-import org.opensaml.xml.encryption.InlineEncryptedKeyResolver
+import java.security.interfaces.RSAPrivateKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.util.Timer
+
+import scala.annotation.migration
+import scala.collection.JavaConverters.asScalaBufferConverter
+
+import org.apache.commons.httpclient.HttpClient
+import org.opensaml.DefaultBootstrap
+import org.opensaml.saml2.core.Assertion
+import org.opensaml.saml2.core.EncryptedAssertion
 import org.opensaml.saml2.core.Response
+import org.opensaml.saml2.encryption.Decrypter
+import org.opensaml.saml2.metadata.provider.HTTPMetadataProvider
+import org.opensaml.saml2.metadata.provider.MetadataProvider
+import org.opensaml.xml.XMLObject
+import org.opensaml.xml.encryption.InlineEncryptedKeyResolver
+import org.opensaml.xml.parse.ParserPool
+import org.opensaml.xml.parse.StaticBasicParserPool
+import org.opensaml.xml.schema.XSString
+import org.opensaml.xml.security.keyinfo.StaticKeyInfoCredentialResolver
+import org.opensaml.xml.security.x509.BasicX509Credential
+import org.slf4j.LoggerFactory
+import org.w3c.dom.bootstrap.DOMImplementationRegistry
+import org.w3c.dom.ls.DOMImplementationLS
+
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
 
 object OpenSaml{
 
 	def setLoggingLevel(): Unit = {
-		import org.slf4j.LoggerFactory;
-		import ch.qos.logback.classic.{Level, Logger};
+		import org.slf4j.LoggerFactory
+		import ch.qos.logback.classic.{Level, Logger}
 
 		LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)
 			.asInstanceOf[Logger]
-			.setLevel(Level.INFO);
+			.setLevel(Level.INFO)
 	}
 
 	setLoggingLevel()
@@ -77,11 +82,15 @@ object OpenSaml{
 	// Create the assertion
 	//val assertion = builder.buildObject()
 	
-	def extractAssertion: Assertion = {
-		import java.io._
+	val encodedPrivateKey: Array[Byte] = {
 		import java.nio.file.{Files, Paths}
-
-		val responseStream = getClass.getResourceAsStream("/response_sample.xml")
+		Files.readAllBytes(Paths.get(getClass.getResource("/private_key.pk8").toURI))
+	}
+	import java.io.InputStream
+	def getResponseStream: InputStream = getClass.getResourceAsStream("/response_sample.xml")
+	
+	def extractAssertions(responseStream: InputStream): Seq[Assertion] = {
+		// Parse the response XML
 		val document = parserPool.parse(responseStream)
 		val metadataRoot = document.getDocumentElement()
 		
@@ -89,12 +98,14 @@ object OpenSaml{
 		val unmarshallerFactory = org.opensaml.xml.Configuration.getUnmarshallerFactory()
 		val unmarshaller = unmarshallerFactory.getUnmarshaller(metadataRoot)
 		val response = unmarshaller.unmarshall(metadataRoot).asInstanceOf[Response]
-		val encryptedAssertion = response.getEncryptedAssertions.get(0)
 		
-		val encodedPrivateKey = Files.readAllBytes(Paths.get(getClass.getResource("/private_key.pk8").toURI))
-		
+		val decryptor = getAssDecryptor(encodedPrivateKey) 
+		response.getEncryptedAssertions.asScala.map(decryptor) ++ response.getAssertions.asScala
+	}
+	
+	def getAssDecryptor(encodedPk8Key: Array[Byte]): EncryptedAssertion => Assertion = {
 		// Create the private key.
-		val privateKeySpec = new PKCS8EncodedKeySpec(encodedPrivateKey)
+		val privateKeySpec = new PKCS8EncodedKeySpec(encodedPk8Key)
 		val privateKey = KeyFactory.getInstance("RSA").generatePrivate(privateKeySpec).asInstanceOf[RSAPrivateKey]
 		
 		// Create the credentials.
@@ -104,6 +115,43 @@ object OpenSaml{
 		// Create a decrypter.
 		val decrypter = new Decrypter(null, new StaticKeyInfoCredentialResolver(decryptionCredential), new InlineEncryptedKeyResolver())
 		
-		decrypter.decrypt(encryptedAssertion)
+		decrypter.decrypt
+	}
+	
+	
+	def extractClasses(xmlObj: XMLObject): Seq[Class[_]] = {
+		if(xmlObj == null)
+			Nil
+		else if(xmlObj.hasChildren)
+			xmlObj.getOrderedChildren.asScala.flatMap(extractClasses)
+		else
+			Seq(xmlObj.getClass)
+	}
+	
+	val domSerializer: org.w3c.dom.ls.LSSerializer = {
+		import  org.w3c.dom.bootstrap.DOMImplementationRegistry
+		import  org.w3c.dom.ls.DOMImplementationLS
+		
+		val registry = DOMImplementationRegistry.newInstance()
+		
+		val domImpl = registry.getDOMImplementation("LS").asInstanceOf[DOMImplementationLS]
+		domImpl.createLSSerializer()
+	}
+	
+	def xmlToStr(xml: org.w3c.dom.Element): String = domSerializer.writeToString(xml)
+	
+	val asses = extractAssertions(getResponseStream).toIndexedSeq
+	
+	def extractAttributes(ass: Assertion): Map[String, Seq[String]] = {
+		import org.opensaml.xml.schema.XSString
+		
+		val attributes = ass.getAttributeStatements.asScala.flatMap(_.getAttributes.asScala)
+		
+		val namesVsValues: Seq[(String, Seq[String])] = for{
+			attr <- attributes;
+			attrVals = attr.getAttributeValues.asScala.collect{ case s: XSString => s.getValue}
+		} yield (attr.getFriendlyName, attrVals)
+		
+		Map(namesVsValues: _*)
 	}
 }
