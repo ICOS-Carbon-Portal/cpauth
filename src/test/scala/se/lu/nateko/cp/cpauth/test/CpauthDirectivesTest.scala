cc8fcb4b80798bd1562fc526e153137cb29c236a
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2015-04-07 17:40:35 +0200

    Tested 'user' directive in CpauthDirectives

--- src/test/scala/se/lu/nateko/cp/cpauth/test/CpauthDirectivesTest.scala
+++ src/test/scala/se/lu/nateko/cp/cpauth/test/CpauthDirectivesTest.scala
@@ -11,18 +11,44 @@ import se.lu.nateko.cp.cpauth.core.Exceptions
 import spray.http.StatusCodes
 import spray.routing.Directives
 import spray.routing.AuthenticationFailedRejection
+import se.lu.nateko.cp.cpauth.core.UrlsConfig
+import se.lu.nateko.cp.cpauth.core.SamlConfig
+import se.lu.nateko.cp.cpauth.core.AuthConfig
+import se.lu.nateko.cp.cpauth.CookieFactory
+import se.lu.nateko.cp.cpauth.core.UserInfo
+import spray.http.HttpHeaders.Cookie
 
 class CpauthDirectivesTest extends FunSpec with ScalatestRouteTest with Directives{
 	
-	def getDirs(pubKeyPath: String, auth: Try[Authenticator]) = new CpauthDirectives{
-		val publicAuthConfig = new PublicAuthConfig{ val publicKeyPath = pubKeyPath}
-		val authenticator = auth
+	def getConfig(privKeyPath: String) = new UrlsConfig with SamlConfig with AuthConfig {
+		def authTokenValiditySeconds: Int = 1000
+		def privateKeyPath: String = privKeyPath
+		
+		// Members declared in se.lu.nateko.cp.cpauth.core.PublicAuthConfig
+		def publicKeyPath: String = "/public1.pem"
+		
+		// Members declared in se.lu.nateko.cp.cpauth.core.SamlConfig
+		def givenNameAttr: String = ???
+		def idpMetadataFilePath: String = ???
+		def mailAttr: String = ???
+		def samlSpXmlPath: String = ???
+		def spConfig = ???
+		def surnameAttr: String = ???
+		
+		// Members declared in se.lu.nateko.cp.cpauth.core.UrlsConfig
+		def drupalProxying = ???
+		def loginPath: String = ???
+		def serviceHost: String = "cpauth.icos-cp.eu"
+		def servicePrivatePort: Int = ???
+	}
+	val config = getConfig("/private1.der")
+	val dirs = new CpauthDirectives{
+		val publicAuthConfig = config
+		val authenticator = Authenticator(publicAuthConfig)
 		implicit val dispatcher = scala.concurrent.ExecutionContext.Implicits.global
 	}
 
 	describe("attempt directive"){
-
-		val dirs = getDirs("", Exceptions.failure("dummy"))
 
 		it("computes inner route if the attempt is successful"){
 			val route = dirs.attempt("blabla"){ s =>
@@ -46,19 +72,49 @@ class CpauthDirectivesTest extends FunSpec with ScalatestRouteTest with Directiv
 		}
 	}
 
-	describe("user directive when no CPauth cookie is present"){
-		val dirs = getDirs("", Exceptions.failure("dummy"))
+	describe("user directive"){
 
-		it("rejects the request with 'CredentialsMissing' rejection"){
-			val route = dirs.user(uinfo => complete(uinfo.givenName))
-			
-			Get("/any") ~> route ~> check{
-				val authRejections = rejections.collect{
-					case AuthenticationFailedRejection(AuthenticationFailedRejection.CredentialsMissing, _) => 1
+		val route = dirs.user(uinfo => complete(uinfo.givenName))
+
+		describe("when no CPauth cookie is present"){
+	
+			it("rejects the request with 'CredentialsMissing' rejection"){
+				
+				Get("/any") ~> route ~> check{
+					val authRejections = rejections.collect{
+						case AuthenticationFailedRejection(AuthenticationFailedRejection.CredentialsMissing, _) => 1
+					}
+					assert(authRejections.length === 1)
 				}
-				assert(authRejections.length === 1)
 			}
 		}
+
+		describe("when a properly signed CPauth cookie is present"){
+			val user = UserInfo("name", "surname", "mail")
+			val cookie = new CookieFactory(config).makeAuthenticationCookie(user).get
+			
+			it("delegates to the inner route"){
+				Get("/any") ~> Cookie(cookie) ~> route ~> check{
+					assert(responseAs[String] === user.givenName)
+				}
+			}
+		}
+
+		describe("when the cookie has been signed with a wrong private key"){
+			val user = UserInfo("name", "surname", "mail")
+			val wrongConfig = getConfig("/saml/test_private_key.der")
+			val cookie = new CookieFactory(wrongConfig).makeAuthenticationCookie(user).get
+			
+			it("rejects the request with 'CredentialsRejected' rejection"){
+				Get("/any") ~> Cookie(cookie) ~> route ~> check{
+					val authRejections = rejections.collect{
+						case AuthenticationFailedRejection(AuthenticationFailedRejection.CredentialsRejected, _) => 1
+					}
+					assert(authRejections.length === 1)
+				}
+			}
+		}
+
 	}
 
 
