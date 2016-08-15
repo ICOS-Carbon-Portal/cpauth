349933e014229ddfea515b20269d5fb3b9ecadfe
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2016-08-15 11:42:02 +0200

    Finilazed the new (Akka) version of ProxyDirectives

--- src/main/scala/se/lu/nateko/cp/cpauth/ProxyDirectives.scala
+++ src/main/scala/se/lu/nateko/cp/cpauth/ProxyDirectives.scala
@@ -1,14 +1,12 @@
 package se.lu.nateko.cp.cpauth
 
-import akka.actor.ActorSystem
-import akka.http.scaladsl.model.headers
+import akka.http.scaladsl.HttpExt
+import akka.http.scaladsl.model.HttpProtocols
+import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.model.Uri
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.RouteResult.Complete
-import akka.http.scaladsl.model.HttpProtocols
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.HttpMessage
-import akka.http.scaladsl.HttpExt
 
 trait ProxyDirectives { this: CpauthDirectives =>
 
@@ -21,19 +19,23 @@ trait ProxyDirectives { this: CpauthDirectives =>
 		val req = ctxt.request
 		val finalPath = if(path.isEmpty) Uri.Path./ else path
 		val newQuery = mergeQueries(query, req.uri.query())
-		val newUri = req.uri.withPath(finalPath).withQuery(newQuery)
-		val newReq = req.copy(uri = newUri, protocol = HttpProtocols.`HTTP/1.1`)
-
+		val newUri = Uri./.withScheme("http").withHost(host).withPort(port).withPath(finalPath).withQuery(newQuery)
+		val newReq = req.copy(uri = newUri, protocol = HttpProtocols.`HTTP/1.1`).withoutRedundantHeaders
 		http.singleRequest(newReq).map(response => Complete(response.withoutRedundantHeaders))
 	}
 }
 
 object ProxyDirectives{
 
-	private val blackList: Set[String] = {
-		import headers._
+	import akka.http.scaladsl.model.headers._
+
+	private val responseBlackList: Set[String] = {
 		Set(`Content-Type`, `Content-Length`, Server, Date, `Transfer-Encoding`)
 			.map(_.lowercaseName)
+	}
+
+	private val requestBlackList: Set[String] = {
+		Set(`Timeout-Access`).map(_.lowercaseName)
 	}
 
 	def mergeQueries(highPrio: Seq[(String, String)], old: Uri.Query) = Uri.Query{
@@ -42,10 +44,19 @@ object ProxyDirectives{
 		}
 	}
 
-	implicit class HeaderManipHttpMessage[T <: HttpMessage](val msg: T) extends AnyVal{
+	implicit class HeaderManipHttpResponse(val msg: HttpResponse) extends AnyVal{
 
 		def withoutRedundantHeaders: msg.Self = {
-			val headers = msg.headers.filter(header => !blackList.contains(header.lowercaseName))
+			val headers = msg.headers.filter(header => !responseBlackList.contains(header.lowercaseName))
+			msg.withHeaders(headers)
+		}
+
+	}
+
+	implicit class HeaderManipHttpRequest(val msg: HttpRequest) extends AnyVal{
+
+		def withoutRedundantHeaders: msg.Self = {
+			val headers = msg.headers.filter(header => !requestBlackList.contains(header.lowercaseName))
 			msg.withHeaders(headers)
 		}
 
