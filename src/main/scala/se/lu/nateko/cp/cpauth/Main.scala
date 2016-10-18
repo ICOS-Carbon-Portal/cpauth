4983d46d2bb5302df9e71f0babdb65e9f1a899b4
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2016-10-18 16:12:06 +0200

    Changed dispatcher for blocking operations. Typo on the login page.

--- src/main/scala/se/lu/nateko/cp/cpauth/Main.scala
+++ src/main/scala/se/lu/nateko/cp/cpauth/Main.scala
@@ -34,7 +34,7 @@ object Main extends App with SamlRouting with PasswordRouting with DrupalRouting
 
 	implicit val system = ActorSystem("cpauth")
 	implicit val dispatcher = system.dispatcher
-	val blockingExeContext  = system.dispatchers.lookup("akka.stream.default-blocking-io-dispatcher")
+	val blockingExeContext  = system.dispatchers.lookup("my-blocking-dispatcher")
 	implicit val scheduler = system.scheduler
 	implicit val materializer = ActorMaterializer(namePrefix = Some("cpauth_mat"))
 
