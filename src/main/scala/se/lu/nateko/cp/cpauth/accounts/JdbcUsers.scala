fce732da1346a61da83247a90ba213dac3758f4b
André Bjärby <andre.bjarby@nateko.lu.se>
2017-01-18 15:51:30 +0100

    Finished work on JDBC-only HQSL access
    The 'JdbcUsers' class now implements the 'UsersIo' interface and is ready to
    replace the current 'Users' object.

--- src/main/scala/se/lu/nateko/cp/cpauth/accounts/JdbcUsers.scala
+++ src/main/scala/se/lu/nateko/cp/cpauth/accounts/JdbcUsers.scala
@@ -1,8 +1,9 @@
 package se.lu.nateko.cp.cpauth.accounts
 
 import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.PreparedStatement
+
+import java.security.MessageDigest
+import java.util.Base64
 
 import scala.collection.mutable.Buffer
 import scala.concurrent.Await
@@ -11,13 +12,11 @@ import scala.concurrent.Future
 import scala.concurrent.duration.Duration
 
 import se.lu.nateko.cp.cpauth.core.UserId
+import se.lu.nateko.cp.cpauth.core.AuthenticationFailedException
 
-class JdbcUsers(implicit ctxt: ExecutionContext){// extends UsersIo{
 
-	Class.forName("org.hsqldb.jdbc.JDBCDataSource")
-
-	def getConnection(): Connection = DriverManager
-		.getConnection("jdbc:hsqldb:file:db/cpauth", "SA", "")
+class JdbcUsers(getConnection: () => Connection)
+			   (implicit ctxt: ExecutionContext) extends UsersIo {
 
 	private def execute(statement: String): Future[Unit] = withConnection{ conn =>
 		val st = conn.createStatement
@@ -26,12 +25,22 @@ class JdbcUsers(implicit ctxt: ExecutionContext){// extends UsersIo{
 	}
 
 	private def withConnection[T](work: Connection => T): Future[T] = Future{
-		val conn = getConnection
+		val conn = getConnection()
 		try{
 			work(conn)
 		} finally{
 			conn.close()
 		}
+	}
+
+	def hash(mail: String, pass: String): String = {
+		val md = MessageDigest.getInstance("MD5")
+		md.update(mail.getBytes("UTF-8"))
+		val salt: Array[Byte] = md.digest
+		val hmd = MessageDigest.getInstance("SHA-256")
+		hmd.update( salt ++ pass.getBytes("UTF-8"))
+		val hashBytes = hmd.digest
+		Base64.getEncoder.encodeToString(hashBytes)
 	}
 
 	Await.ready(createTable(), Duration.Inf)
@@ -52,22 +61,47 @@ class JdbcUsers(implicit ctxt: ExecutionContext){// extends UsersIo{
 		execute(sql)
 	}
 
+	def addUser(userEntry: UserEntry, password: String): Future[Unit] = withConnection{ conn =>
+		val ps = conn.prepareStatement("insert into users (mail,password,isadmin) values(?,?,?)")
 
-	def addUser(userEntry: UserEntry, password: String): Future[Unit] = Future{
-		val ps: PreparedStatement = getConnection.prepareStatement(
-			"""INSERT INTO "USERS" ("MAIL", "PASSWORD", "ISADMIN") VALUES (?, ?, ?)"""
-		)
-		try{
-			val uinfo = userEntry.id
-			val passHash = Users.hash(uinfo.email, password)
+		ps.setString(1, userEntry.id.email)
+		ps.setString(2, hash(userEntry.id.email, password))
+		ps.setBoolean(3, userEntry.isAdmin)
+		ps.execute()
+	}
 
-			ps.setString(2, uinfo.email)
-			ps.setString(3, passHash)
-			ps.setBoolean(4, userEntry.isAdmin)
-			ps.execute()
-		}finally{
-			ps.getConnection().close()
+	def userExists(uid: UserId): Future[Boolean] = withConnection { conn =>
+		val ps = conn.prepareStatement("select 1 from users where mail = ?")
+		ps.setString(1, uid.email)
+		ps.executeQuery().next()
+	}
+
+	def authenticateUser(uid: UserId, password: String): Future[UserEntry] = withConnection { conn =>
+		val ps = conn.prepareStatement("select isAdmin from users where mail = ? and password = ?")
+		ps.setString(1, uid.email)
+		ps.setString(2, hash(uid.email, password))
+
+		val rs = ps.executeQuery()
+		if (rs.next()) {
+			UserEntry(uid, rs.getBoolean("isAdmin"))
 		}
+		else {
+			throw AuthenticationFailedException
+		}
+	}
+
+	def updateUser(oldUid: UserId, ue: UserEntry, newPass: String):Future[Unit] = withConnection { conn =>
+		val ps = conn.prepareStatement("update users set isAdmin=?, password=? where mail=?")
+		ps.setBoolean(1, ue.isAdmin)
+		ps.setString(2, hash(ue.id.email, newPass))
+		ps.setString(3, oldUid.email)
+		ps.executeUpdate()
+		}
+
+	def dropUser(uid: UserId): Future[Unit] = withConnection { conn =>
+		val ps = conn.prepareStatement("delete from users where mail = ?")
+		ps.setString(1, uid.email)
+		ps.executeUpdate()
 	}
 
 	def listUsers: Future[Seq[UserEntry]] = withConnection{conn =>
@@ -81,4 +115,19 @@ class JdbcUsers(implicit ctxt: ExecutionContext){// extends UsersIo{
 		}
 		result
 	}
+
+	def userIsAdmin(uid: UserId) = withConnection { conn =>
+		val ps = conn.prepareStatement("select 1 from users where mail = ? and isAdmin")
+		ps.setString(1, uid.email)
+		ps.executeQuery().next()
+	}
+
+	def setAdminRights(uid: UserId, isAdmin: Boolean): Future[Unit] = withConnection { conn =>
+		val ps = conn.prepareStatement("update users set isAdmin = ? where mail = ?")
+		ps.setBoolean(1, isAdmin)
+		ps.setString(2, uid.email)
+		ps.executeUpdate()
+	}
+
+
 }
---
+++ src/test/scala/se/lu/nateko/cp/cpauth/test/JdbcUsersTest.scala
@@ -0,0 +1,82 @@
+package se.lu.nateko.cp.cpauth.test
+
+import java.sql.DriverManager
+import org.scalatest.FunSuite
+import org.scalatest.concurrent.ScalaFutures
+import scala.concurrent.ExecutionContext.Implicits.global
+import se.lu.nateko.cp.cpauth.accounts._
+import se.lu.nateko.cp.cpauth.core._
+import se.lu.nateko.cp.cpauth.core.AuthenticationFailedException
+
+class JdbcUsersTest extends FunSuite with ScalaFutures {
+
+	def getConnection() = {
+		Class.forName("org.hsqldb.jdbc.JDBCDataSource")
+		DriverManager.getConnection("jdbc:hsqldb:mem:test")
+	}
+
+	test("Simple test of all operations") {
+		val jdu = new se.lu.nateko.cp.cpauth.accounts.JdbcUsers(getConnection)
+		val uid = UserId("ordinaryuser")
+		val ue  = UserEntry(uid, false)
+
+		// There are no users.
+		assertResult(Seq())   { jdu.listUsers.futureValue }
+		// There really isn't.
+		assertResult(false)	  { jdu.userExists(uid).futureValue }
+		// But it should be ok to add one.
+		assertResult( () )	  { jdu.addUser(ue, "password").futureValue }
+		// Once the user has been added there should be users to list.
+		assertResult(Seq(ue)) { jdu.listUsers.futureValue }
+		assertResult(true)	  { jdu.userExists(uid).futureValue }
+		assertResult(false)   { jdu.userIsAdmin(uid).futureValue }
+		// And to authenticate
+		assertResult(ue)	  { jdu.authenticateUser(uid, "password").futureValue }
+		// But only using the correct password.
+		val a = jdu.authenticateUser(uid, "wrong!")
+		assert(a.failed.futureValue.isInstanceOf[AuthenticationFailedException.type])
+
+		// Once we drop the only user, it's should be gone.
+		jdu.dropUser(uid).futureValue
+		assertResult(false)   { jdu.userExists(uid).futureValue }
+		assertResult(Seq())   { jdu.listUsers.futureValue }
+
+		// Of course, with no users, there is no user to authenticate.
+		val b = jdu.authenticateUser(uid, "password")
+		assert(b.failed.futureValue.isInstanceOf[AuthenticationFailedException.type])
+
+		// Let's set up another user
+		val aid = UserId("admin")
+		val aue = UserEntry(aid, true)
+		// And then add both users
+		assertResult( () )	  { jdu.addUser(ue, "password").futureValue }
+		assertResult( () )	  { jdu.addUser(aue, "password").futureValue }
+		// Now there should be two users
+		assertResult( 2 ) { jdu.listUsers.futureValue.length }
+		// And one of them is admin
+		assertResult(true)    { jdu.userIsAdmin(aid).futureValue }
+		// Remove admin rights
+		jdu.setAdminRights(aid, false).futureValue
+		assertResult(false)    { jdu.userIsAdmin(aid).futureValue }
+		// And add them back again
+		jdu.setAdminRights(aid, true).futureValue
+		assertResult(true)    { jdu.userIsAdmin(aid).futureValue }
+
+		// You can make nonexisting users admin.
+		val c = UserId("nosuchuser")
+		jdu.setAdminRights(c, true).futureValue
+		// But it doesn't let them authenticate
+		assertResult(false)    { jdu.userIsAdmin(c).futureValue }
+
+		// Check password
+		assertResult(ue) { jdu.authenticateUser(uid, "password").futureValue }
+		// Then change password
+		jdu.updateUser(uid, ue, "newpassword").futureValue
+		// The old password should no longer authenticate
+		val d = jdu.authenticateUser(uid, "password")
+		assert(d.failed.futureValue.isInstanceOf[AuthenticationFailedException.type])
+		// But the new one should
+		assertResult(ue) { jdu.authenticateUser(uid, "newpassword").futureValue }
+
+	}
+}
