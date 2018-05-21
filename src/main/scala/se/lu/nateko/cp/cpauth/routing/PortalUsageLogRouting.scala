96b0ddd71ded527ef40c0abfb16157b883ce14b9
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2018-05-21 11:46:37 +0200

    Access-Control headers for the /portaluse service

--- src/main/scala/se/lu/nateko/cp/cpauth/routing/PortalUsageLogRouting.scala
+++ src/main/scala/se/lu/nateko/cp/cpauth/routing/PortalUsageLogRouting.scala
@@ -1,12 +1,13 @@
 package se.lu.nateko.cp.cpauth.routing
 
-import se.lu.nateko.cp.cpauth.services.PortalUsageLogger
-import akka.http.scaladsl.server.Directives._
-import spray.json.JsValue
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.HttpMethods
 import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.model.headers.`X-Real-Ip`
-import akka.http.scaladsl.server.{Directive1, MissingHeaderRejection, RejectionHandler}
+import akka.http.scaladsl.model.headers._
+import akka.http.scaladsl.server.{ Directive1, MissingHeaderRejection, RejectionHandler }
+import akka.http.scaladsl.server.Directives._
+import se.lu.nateko.cp.cpauth.services.PortalUsageLogger
+import spray.json.JsValue
 
 
 trait PortalUsageLogRouting extends CpauthDirectives{
@@ -14,10 +15,23 @@ trait PortalUsageLogRouting extends CpauthDirectives{
 	def usageLogger: PortalUsageLogger
 
 	val usageLogRouting = extractEnvri{implicit envri =>
-		(post & path("portaluse")){
-			getClientIp{ip =>
-				entity(as[JsValue]){js =>
-					usageLogger.log(js.asJsObject, ip)
+		path("portaluse"){
+			post{
+				respondWithHeaders(`Access-Control-Allow-Origin`.*){
+					getClientIp{ip =>
+						entity(as[JsValue]){js =>
+							usageLogger.log(js.asJsObject, ip)
+							complete(StatusCodes.OK)
+						}
+					}
+				}
+			} ~
+			options{
+				respondWithHeaders(
+					`Access-Control-Allow-Origin`.*,
+					`Access-Control-Allow-Methods`(HttpMethods.POST),
+					`Access-Control-Allow-Headers`("Content-Type")
+				){
 					complete(StatusCodes.OK)
 				}
 			}
