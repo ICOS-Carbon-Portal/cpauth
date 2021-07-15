58b7f115bf5ae2b805c292205072b0babe73e312
Oleg Mirzov <oleg.mirzov@gmail.com>
2021-07-15 17:06:10 +0200

    Fix emailing by lib upgrade, revert previous commit
    This reverts commit bace36742f35bdc2de9c714d21463e6aed4bd16c.

--- src/main/scala/se/lu/nateko/cp/cpauth/CpauthConfig.scala
+++ src/main/scala/se/lu/nateko/cp/cpauth/CpauthConfig.scala
@@ -86,8 +86,6 @@ case class CpGeoConfig(baseUri: String, maxAgeDays: Int, emailErrorsTo: String)
 
 case class EmailConfig(
 	smtpServer: String,
-	starttls: Boolean,
-	smtpPort: Int,
 	username: String,
 	password: String,
 	fromAddress: String,
@@ -161,7 +159,7 @@ object ConfigReader extends DefaultJsonProtocol{
 	implicit val restHeartConfigFormat = jsonFormat5(RestHeartConfig)
 	implicit val credentialsConfigFormat = jsonFormat2(CredentialsConfig)
 	implicit val postgresConfigFormat = jsonFormat5(PostgresConfig)
-	implicit val emailConfigFormat = jsonFormat7(EmailConfig)
+	implicit val emailConfigFormat = jsonFormat5(EmailConfig)
 	implicit val oauthProviderConfigFormat = jsonFormat3(OAuthProviderConfig)
 	implicit val geoConfigFormat = jsonFormat3(CpGeoConfig)
 
@@ -177,5 +175,5 @@ object ConfigReader extends DefaultJsonProtocol{
 	}
 
 	implicit val envriOAuthConfigFormat = implicitly[JsonFormat[CpauthConfig.EnvriOAuthConfig]]
-
+	
 }
