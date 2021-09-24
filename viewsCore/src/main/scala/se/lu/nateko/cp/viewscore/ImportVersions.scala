54be4d685f8a9c1a71549f0daf0fff5f893d12ff
Jonathan Thiry <github@jonathanthiry.com>
2021-09-24 16:12:07 +0200

    [views-core] Add support for Bootstrap JS

---
+++ viewsCore/src/main/scala/se/lu/nateko/cp/viewscore/ImportVersions.scala
@@ -0,0 +1,8 @@
+package se.lu.nateko.cp.viewscore
+
+object ImportVersions {
+	val bootstrapCSS = "5.1.0"
+	val bootstrapJS = "5.1.1"
+	val bootstrapJSIntegrity = "sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
+	val fontAwesome = "5.15.3"
+}
