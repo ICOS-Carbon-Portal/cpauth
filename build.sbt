d677d070d7d824cdfae2a5f5f1536cb5a6fdf5c6
Jonathan Thiry <github@jonathanthiry.com>
2023-08-21 14:30:05 +0200

    [views-core] Upgrade to Bootstrap 5.3.1

--- build.sbt
+++ build.sbt
@@ -62,7 +62,7 @@ lazy val viewsCore = (project in file("viewsCore"))
 	.enablePlugins(SbtTwirl)
 	.settings(
 		name := "views-core",
-		version := "0.7.2",
+		version := "0.7.3",
 	)
 
 val akkaVersion = "2.6.19"
--- viewsCore/src/main/scala/se/lu/nateko/cp/viewscore/ImportVersions.scala
+++ viewsCore/src/main/scala/se/lu/nateko/cp/viewscore/ImportVersions.scala
@@ -1,8 +1,8 @@
 package se.lu.nateko.cp.viewscore
 
 object ImportVersions {
-	val bootstrapCSS = "5.2.1"
-	val bootstrapJS = "5.2.1"
-	val bootstrapJSIntegrity = "sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+	val bootstrapCSS = "5.3.1"
+	val bootstrapJS = "5.3.1"
+	val bootstrapJSIntegrity = "sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
 	val fontAwesome = "5.15.3"
 }
