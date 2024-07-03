ad38893bbd3f296ba38b38745324206b0a5fb1a9
Jonathan Thiry <github@jonathanthiry.com>
2024-07-03 18:58:48 +0200

    [views-core] Update bootstrap

--- viewsCore/src/main/scala/se/lu/nateko/cp/viewscore/ImportVersions.scala
+++ viewsCore/src/main/scala/se/lu/nateko/cp/viewscore/ImportVersions.scala
@@ -1,8 +1,8 @@
 package se.lu.nateko.cp.viewscore
 
 object ImportVersions {
-	val bootstrapCSS = "5.3.2"
-	val bootstrapJS = "5.3.2"
-	val bootstrapJSIntegrity = "sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+	val bootstrapCSS = "5.3.3"
+	val bootstrapJS = "5.3.3"
+	val bootstrapJSIntegrity = "sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
 	val fontAwesome = "5.15.3"
 }
